9249fffcf6f18ab3d467f1d500cab573efd3c9a4
Marion BERNARD <marion.lautier.bernard@gmail.com>
2023-10-06 12:19:20 +0200

    maj menu responsive

--- js/script.js
+++ js/script.js
@@ -1,11 +1,14 @@
 const toggleMenu = document.querySelector(".toggle-menu")
 const menu = document.querySelector(".menu")
+const menuBordure = document.querySelector(".menu-bordure")
+
 console.log(toggleMenu)
 
 toggleMenu.addEventListener("click",function(){
     const open = JSON.parse(toggleMenu.getAttribute('aria-expanded')) /*Pour avoir la valeur et pas la string*/
     toggleMenu.setAttribute('aria-expanded',!open) /* si Aria expanded true -> false, et vice-versa*/
     menu.toggleAttribute("hidden");
+    menuBordure.toggleAttribute("hidden")
 
     if (!menu.hidden) {
             menu.querySelector('a').focus();
