e4b9aa85d28b6eca5db060e9c70cfd658e651393
Marion BERNARD <marion.lautier.bernard@gmail.com>
2023-10-10 15:47:18 +0200

    aj menu

--- js/script.js
+++ js/script.js
@@ -1,15 +1,16 @@
 const toggleMenu = document.querySelector(".toggle-menu")
 const menu = document.querySelector(".menu")
-const menuBordure = document.querySelector(".menu-bordure")
-
-console.log(toggleMenu)
+// const menuBordure = document.querySelector(".menu-bordure")
+// const menuComplet = document.querySelector(".menu-complet")
 
 toggleMenu.addEventListener("click",function(){
     const open = JSON.parse(toggleMenu.getAttribute('aria-expanded')) /*Pour avoir la valeur et pas la string*/
     toggleMenu.setAttribute('aria-expanded',!open) /* si Aria expanded true -> false, et vice-versa*/
     menu.toggleAttribute("hidden");
-    menuBordure.toggleAttribute("hidden")
+    // menuBordure.toggleAttribute("hidden")
+    // menuComplet.toggleAttribute("hidden")
 
+ 
     if (!menu.hidden) {
             menu.querySelector('a').focus();
           }
