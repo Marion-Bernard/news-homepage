b3346d567d1b22713ee858a82df99cbfc26711cc
Marion BERNARD <marion.bernard2025@campus-eni.fr>
2025-12-19 09:40:08 +0100

    supression commentaire inutile et ajout de style

--- js/script.js
+++ js/script.js
@@ -1,14 +1,10 @@
 const toggleMenu = document.querySelector(".toggle-menu")
 const menu = document.querySelector(".menu")
-// const menuBordure = document.querySelector(".menu-bordure")
-// const menuComplet = document.querySelector(".menu-complet")
 
 toggleMenu.addEventListener("click",function(){
     const open = JSON.parse(toggleMenu.getAttribute('aria-expanded')) /*Pour avoir la valeur et pas la string*/
     toggleMenu.setAttribute('aria-expanded',!open) /* si Aria expanded true -> false, et vice-versa*/
     menu.toggleAttribute("hidden");
-    // menuBordure.toggleAttribute("hidden")
-    // menuComplet.toggleAttribute("hidden")
 
  
     if (!menu.hidden) {
